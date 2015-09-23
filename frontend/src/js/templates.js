cafa158d81c83a4dc2b1796d1d59cbf9a4ca6fe6
soylent-grin <hocico16@gmail.com>
2015-09-23 18:07:13 +0300

    Forgotten template file

---
+++ frontend/src/js/templates.js
@@ -0,0 +1,68 @@
+"use strict";
+
+import _ from 'lodash';
+import uuid from 'uuid';
+
+export function ClassTemplate() {
+    return {
+        "@type": "prov:Entity",
+        "rdfs:label": {
+            "@language": "en",
+            "@value": ""
+        },
+        "rdfs:subClassOf": "ssn:System",
+        "ssn:hasSubSystem": [
+
+        ],
+        uri: "_:" + uuid.v4()
+    };
+}
+
+export function SensorTemplate() {
+    return {
+        "@type": "ssn:Sensor",
+        "ssn:hasMeasurementCapability": {
+            "@type": "ssn:MeasurementCapability",
+            "ssn:forProperty": "someprefix:AirTemperature", // TODO: make this dynamically
+            "ssn:hasMeasurementProperty": [
+
+            ],
+            "uri": "_:" + uuid.v4()
+        },
+        "ssn:observes": "someprefix:AirTemperature",
+        "uri": "_:" + uuid.v4()
+    };
+}
+
+export const PropertyTemplate = {
+    'Sensitivity': function() {
+        return {
+            "@type": "ssn:Sensitivity",
+            "ssn:hasValue": {
+                "@type": "DUL:Amount",
+                "DUL:hasDataValue": {
+                    "xsd1:double": "",
+                    uri: "_:" + uuid.v4()
+                },
+                "DUL:isClassifiedBy": "",
+                uri: "_:" + uuid.v4()
+            },
+            uri: "_:" + uuid.v4()
+        };
+    },
+    'Accuracy': function() {
+        return {
+            "@type": "ssn:Accuracy",
+            "ssn:hasValue": {
+                "@type": "DUL:Amount",
+                "DUL:hasDataValue": {
+                    "xsd1:double": "{0}",
+                    uri: "_:" + uuid.v4()
+                },
+                "DUL:isClassifiedBy": "{1}",
+                uri: "_:" + uuid.v4()
+            },
+            uri: "_:" + uuid.v4()
+        };
+    }
+};
